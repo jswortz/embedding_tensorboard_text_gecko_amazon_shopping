e2da616d1980cbc1b39df34c4ae0cbb3f0a00ac9
jwortz <jwortz@google.com>
2024-03-23 18:51:27 +0000

    gcsfuse

--- new_env.sh
+++ new_env.sh
@@ -7,6 +7,14 @@
 conda create -n $1 -y
 conda activate $1
 
+#install gcs fuse
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install fuse gcsfuse
+gcloud auth application-default login
+
 
 # Install packages using a pip local to the conda environment.
 conda install pip ipykernel -y
